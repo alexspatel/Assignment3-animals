c46532d293efd9c9bd20792a5d4f7010f19d3312
alexspatel <alexspatel@gmail.com>
2014-03-27 12:16:45 -0600

    Create animals.h

---
+++ animals.h
@@ -0,0 +1,33 @@
+/*
+		FILE: animals.h
+		AUTHOR: Alex Patel
+
+		For CSCI 262, Spring 2014, Assignment 2
+
+		Header file for the game, 20 questions, with subject: animals
+*/
+
+#ifndef ANIMALS_H
+#define ANIMALS_H
+
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+template <class T>
+class binary_tree {
+public:
+	T data;
+	binary_tree_node<T>* left;
+	binary_tree_node<T>* right;
+	binary_tree_node<T>* root;
+
+	string play_game(binary_tree_node<string>* root);
+	string save_game_tree(binary_tree_node<string>* root);
+	string delete_game_tree(binary_tree_node<string>* root);
+	void read_game_tree();
+private:
+	node<string>* root;
+};
+#endif
