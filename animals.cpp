06829731ecfbf5084d019732ac3cc335696d9b28
alexspatel <alexspatel@gmail.com>
2014-04-02 14:02:29 -0600

    Update animals.cpp

--- animals.cpp
+++ animals.cpp
@@ -1,115 +1,44 @@
 /*
-	FILE: animals.cpp
-	AUTHOR: Alex Patel
 
-	For CSCI 262, Spring 2014, Assignment 3
+	  FILE: useful.cpp
 
-	Implementation file for the game 20 questions, with subject: animals
+	  AUTHOR: Alex Patel
+
+	  For CSCI 262 Spring 2014, Assignment 3, 20 questions  
 
 */
 
-#include "animals.h"
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
 #include <cstdlib>
-#include <queue>
-
+#include "animal2.h"
 
 using namespace std;
 
 
-
-template <class T>
-binary_tree_node<T>* create_node(const T& entry,binary_tree_node<T>* L_ptr = NULL,binary_tree_node<T>* R_ptr = NULL)
+// takes user input for an animal
+void eat_line()
 {
-	binary_tree_node<T> *result_ptr;
-
-	result_ptr = new binary_tree_node<T>;
-	result_ptr->data = entry;
-	result_ptr->left = L_ptr;
-	result_ptr->right - R-ptr;
-
-	return result_ptr;
+	char next;
+	
+	do {
+		cin.get(next);
+	} while (next != '\n');
 }
 
-
-template <class T>
-bool is_leaf(const binary_tree_node<T>& node)
+// takes user input for y/n
+bool inquire(const char query[])
 {
-	return (node.left == NULL) && (node.right == NULL);
-}
+	char answer;
 
-
-
-template <class T>
-void tree_clear(const binary_tree_node<T>& node)
-{
-	if(root_ptr != NULL)
+	do
 	{
-		tree_clear(root_ptr->left);
-		tree_clear(root_ptr->right);
-		delete root_ptr;
+		cout << query << " [y/n]" << endl;
+		cin >> answer;
+		cout << endl;
+		answer = toupper(answer); // convert lowercase to uppercase
+		eat_line(); // call eat_line function
 	}
+	while ((answer != 'Y') && (answer != 'N'));
+	return (answer == 'Y');
 }
 
-
-
-template <class T>
-binary_tree_node<T>* tree_copy(binary_tree_node<T>* root_ptr)
-{
-	binary_tree_node<T> *l_ptr;
-	binary_tree_node<T> *r_ptr;
-	if( root_ptr == NULL )
-	{
-		return NULL;
-	}
-	else
-	{
-		l_ptr = tree_copy(root_ptr->left);
-		r_ptr = tree_copy(root_ptr->right);
-		return create_node(root_ptr->data, l_ptr, r_ptr);
-	}
-}
-
-template <class T>
-void remove(binary_tree_node<T>*& root, T val)
-{
-	if( root == NULL ) return NULL; // base case: item is not found
-
-	// Find the item to delete, call remove recursively
-	if( val < root->data) remove(root->left, val);
-	else if( val > root->data) remove(root->right, val);
-
-	// Case 1 and 2, no children and one child
-	else
-	{
-		if( root->left == NULL || root->right == NULL )
-		{
-			binary_tree_node<T>* temp;
-			if( root->left == NULL ) temp = root->right;
-			else temp = root->left;
-			delete root;
-			root = temp;
-		}
-		// case 3, two children
-		else 
-		{
-			binary_tree_node<T>* temp = root->left; 
-			binary_tree_node<T>* parent = root; 
-
-			while( temp->right != NULL )
-			{
-				parent = temp;
-				temp = temp->right;
-			}
-			root->data = temp->date;
-
-			if( parent == root ) root->left = temp->left;
-			else parent->right = temp->left;
-
-			delete temp;
-		}
-	}
-}
