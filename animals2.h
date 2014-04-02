30631a26f48cbaae03cd37ec60eadfedb40787b5
alexspatel <alexspatel@gmail.com>
2014-04-02 14:03:11 -0600

    Create animals2.h

---
+++ animals2.h
@@ -0,0 +1,17 @@
+/*
+
+	  FILE: useful.h
+
+	  AUTHOR: Alex Patel
+
+	  For CSCI 262 Spring 2014, Assignment 3, 20 questions  
+
+*/
+
+#ifndef ANIMALS2_H
+#define ANIMALS2_H
+
+void eat_line();
+bool inquire(const char query[]);
+
+#endif
